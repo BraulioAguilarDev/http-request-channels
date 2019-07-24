5ee68b19f4b45083b4b171fe141398a62e25ea50
Braulio Aguilar <braulio.inf@gmail.com>
2019-07-24 00:50:47 -0500

    concurrently

--- main.go
+++ main.go
@@ -2,31 +2,69 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"time"
 )
 
-func MakePetition(url string, ch chan<- string) {
-	start := time.Now()
-	res, _ := http.Get(url)
+var urls []string
 
-	secs := time.Since(start).Seconds()
-	body, _ := ioutil.ReadAll(res.Body)
-	ch <- fmt.Sprintf("%.2f elapsed with response length: %d %s", secs, len(body), url)
+func init() {
+	for i := 0; i <= 30; i++ {
+		urls = append(urls, fmt.Sprintf("https://reqres.in/api/users/%v", i))
+	}
+
+}
+
+type HttpResponse struct {
+	Url      string
+	Response *http.Response
+	Err      error
+}
+
+func AsyncHttpGets(urls []string) []*HttpResponse {
+	ch := make(chan *HttpResponse)
+	responses := []*HttpResponse{}
+	client := http.Client{}
+
+	for _, url := range urls {
+		go func(url string) {
+			fmt.Printf("Fetching ==> %s \n", url)
+
+			resp, err := client.Get(url)
+			ch <- &HttpResponse{url, resp, err}
+
+			if err != nil && resp != nil && resp.StatusCode == http.StatusOK {
+				resp.Body.Close()
+			}
+		}(url)
+	}
+
+	for {
+		select {
+		case r := <-ch:
+			fmt.Printf("%s was fetched \n", r.Url)
+			if r.Err != nil {
+				fmt.Println("xxx With an error xxx", r.Err)
+			}
+
+			responses = append(responses, r)
+			if len(responses) == len(urls) {
+				return responses
+			}
+		case <-time.After(50 * time.Millisecond):
+			fmt.Println(".")
+		}
+	}
+
+	return responses
 }
 
 func main() {
-	urls := []string{"https://reqres.in/api/users/1", "https://reqres.in/api/users/2", "https://reqres.in/api/users/3"}
-	start := time.Now()
-	ch := make(chan string)
-	for _, url := range urls {
-		go MakePetition(url, ch)
-	}
+	results := AsyncHttpGets(urls)
 
-	for range urls {
-		fmt.Println(<-ch)
+	for key, result := range results {
+		if result != nil && result.Response != nil {
+			fmt.Printf("%v ==> %s\n", key, result.Response.Status)
+		}
 	}
-
-	fmt.Printf("%.2fs elapsed \n", time.Since(start).Seconds())
 }
